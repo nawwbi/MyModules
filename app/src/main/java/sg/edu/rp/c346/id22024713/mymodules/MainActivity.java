17e93915453da15818305aceac9f89e0a27b1370
22024713 <22024713@myrp.edu.sg>
2023-05-23 22:55:34 +0800

    edit 22:55

--- app/src/main/java/sg/edu/rp/c346/id22024713/mymodules/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22024713/mymodules/MainActivity.java
@@ -10,16 +10,20 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv;
+    TextView c346mod, c206mod, c203mod, c218mod, c235mod;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = findViewById(R.id.modules);
+        c346mod = findViewById(R.id.c346);
+        c206mod = findViewById(R.id.c206);
+        c203mod = findViewById(R.id.c203);
+        c218mod = findViewById(R.id.c218);
+        c235mod = findViewById(R.id.c235);
 
-        tv.setOnClickListener(new View.OnClickListener() {
+        c346mod.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AllActivity.class);
@@ -33,6 +37,61 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        c206mod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AllActivity.class);
+                intent.putExtra("Code", "C206");
+                intent.putExtra("Name", "Software Development");
+                intent.putExtra("Year",2023);
+                intent.putExtra("Sem", 1);
+                intent.putExtra("Credit", 4);
+                intent.putExtra("Venue", "W64P");
+                startActivity(intent);
+            }
+        });
+
+        c203mod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AllActivity.class);
+                intent.putExtra("Code", "C203");
+                intent.putExtra("Name", "Web App Development in PHP");
+                intent.putExtra("Year",2023);
+                intent.putExtra("Sem", 1);
+                intent.putExtra("Credit", 4);
+                intent.putExtra("Venue", "W64N");
+                startActivity(intent);
+            }
+        });
+
+        c218mod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AllActivity.class);
+                intent.putExtra("Code", "C218");
+                intent.putExtra("Name", "UI/UX Design for Apps");
+                intent.putExtra("Year",2023);
+                intent.putExtra("Sem", 1);
+                intent.putExtra("Credit", 4);
+                intent.putExtra("Venue", "W64P");
+                startActivity(intent);
+            }
+        });
+
+        c235mod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AllActivity.class);
+                intent.putExtra("Code", "C235");
+                intent.putExtra("Name", "IT Security and Management");
+                intent.putExtra("Year",2023);
+                intent.putExtra("Sem", 1);
+                intent.putExtra("Credit", 4);
+                intent.putExtra("Venue", "W65A");
+                startActivity(intent);
+            }
+        });
 
     }
 }
